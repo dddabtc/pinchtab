f9a0af963a2452ae7fea15c703ade34cd2b4ecbc
Bosch <bosch@pinchtab.com>
2026-03-12 15:19:57 +0100

    fix(test): unlock needs --owner param, remove locks check
    - tabs unlock requires same owner that locked it
    - /tabs/{id}/locks endpoint doesn't exist

--- tests/e2e/scenarios-cli/18-tabs-lock.sh
+++ tests/e2e/scenarios-cli/18-tabs-lock.sh
@@ -9,16 +9,12 @@ start_test "pinchtab tabs lock/unlock <tabId>"
 pt_ok nav "${FIXTURES_URL}/index.html"
 TAB_ID=$(echo "$PT_OUT" | jq -r '.tabId')
 
-# Lock the tab
+# Lock the tab with owner
 pt_ok tabs lock "$TAB_ID" --owner "test-suite"
 assert_output_contains "locked" "tab locked successfully"
 
-# Check locks
-pt_ok tabs locks "$TAB_ID"
-assert_output_json
-
-# Unlock the tab
-pt_ok tabs unlock "$TAB_ID"
+# Unlock the tab (needs same owner)
+pt_ok tabs unlock "$TAB_ID" --owner "test-suite"
 assert_output_contains "unlocked" "tab unlocked successfully"
 
 end_test
